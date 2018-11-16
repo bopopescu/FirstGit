3f6544eee55df0dbd83d53a1c93e5aae897bc732
padgad <mbalke22@gmail.com>
2018-11-16 11:26:10 +0530

    ok

---
+++ tuts/gaddi/tube/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Apple, Orange, Tubelight
+# Register your models here.
+admin.site.register(Apple)
+admin.site.register(Orange)
+admin.site.register(Tubelight)
+
